caa5b551064581255343beb85017ee1dc4392e8c
apiszczukw <apiszczukw@gmail.com>
2022-04-02 12:17:58 +0200

    CSFix

--- TISklep/Startup.cs
+++ TISklep/Startup.cs
@@ -27,7 +27,7 @@ namespace TISklep
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<FilmyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Test")));
+            services.AddDbContext<FilmyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("FilmyCS")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
